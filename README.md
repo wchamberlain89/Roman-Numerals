835ff8eec59c0d87846c16aca8bce9f4e56ba0f9
Dino Nguyen and Ward Chamberlain <dino@>
2019-01-24 15:13:27 -0800

    Add conversion functionality (uncomplete)

---
+++ README.md
@@ -0,0 +1,23 @@
+convert only symbol values to letters
+
+it will convert 1 to I
+it will convert V to 5
+it will convert X to 10
+etc....
+
+convert any number to a set of roman numerals that add to the number.
+
+it will convert 4 --> IIII
+it will covert 9 --> VIIII
+it will convert 20 --> XX
+
+convert number not including 4 same characters in a result.
+
+it will convert 4 --> IV
+it will convert 9 --> IX
+it will convert 90 --> XC instead of LXXXX
+
+convert number including separation of tenths
+
+it will convert 99 --> XCIX
+it will convert 980 --> CMLXXX
---
+++ js/scripts.js
@@ -0,0 +1,45 @@
+// business logic
+
+function getRomanQuantities(num) {
+  var remainingValue = num;
+  var values = [];
+  var romanValues = [1000, 500, 100, 50, 10, 5, 1];
+
+  romanValues.forEach(function(value){
+    var amountOfTimes = Math.floor(remainingValue / value);
+    if (amountOfTimes > 0) {
+      remainingValue -= (amountOfTimes * value);
+    }
+    values.push(amountOfTimes);
+  });
+
+  return values;
+};
+
+// function romanConverter(number) {
+//   var romanLetters = ["M", "D", "C", "L", "X", "V", "I"];
+//   var romanValues = [1000, 500, 100, 50, 10, 5, 1];
+//   var index = romanValues.indexOf(number);
+//   return romanLetters[index];
+// }
+
+function romanDisplay(values) {
+  var romanLetters = ["M", "D", "C", "L", "X", "V", "I"];
+  var romanVals = getRomanQuantities(values);
+  var result = "";
+  romanVals.forEach(function(val, index){
+    for (var i = 0; i < val; i++) {
+      result += romanLetters[index];
+    }
+  });
+  return result;
+};
+
+
+// user interface
+$(function(){
+  $("button").click(function(){
+    var result = romanConverter(parseInt($("#userNumber").val()));
+    $("#result").text(result);
+  });
+});
